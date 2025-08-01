179a9a2f27c1b3c2ac788324eb1eef45d87d7090
Timothy Assi <timothyassi@gmail.com>
2025-08-01 22:51:35 +0200

    Force commit: Update Firebase API key for deploy test

---
+++ js/firebase-config-env.js
@@ -0,0 +1,8 @@
+// Firebase Environment Configuration
+// This file is auto-generated from environment variables
+// Do not edit manually - run 'npm run generate-firebase-config' instead
+
+export const firebaseEnvConfig = {
+    apiKey: "AIzaSyBMssl0NNN-RGhSUqAErsHM1TpkVFT6cvc",
+    // Other config values are loaded from firebase-config-real.js
+};
